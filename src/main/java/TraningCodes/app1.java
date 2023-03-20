699ee9248ff6ea9a1978e480bd11fb2ae002bd24
apoor <apoor@LAPTOP-S27JHK7J>
2023-03-20 10:06:57 +0530

    old programs added

---
+++ src/main/java/TraningCodes/app1.java
@@ -0,0 +1,34 @@
+package TraningCodes;
+import java.util.Scanner;
+public class app1 {
+
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+				System.out.println(" enter the value of n:");
+				
+				int n=sc.nextInt();
+				for(int i=1; i<=n; i++) {
+					for(int j=1; j<=n; j++) {
+						System .out.print("$ ");
+					}
+					for(int j=1; j<=n; j++) {
+						System.out.print("# ");
+					}
+					System.out.println();
+				}
+	}
+}
+	
+
+
+					
+				
+
+			
+
+
+
+
+	
+
+
